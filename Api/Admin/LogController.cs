7209dd73df97b7651c03c09ce0d87e3ff5e32ecf
Saeed Nemati <sn.1361@gmail.com>
2021-10-26 08:31:58 +0330

    committed by script

---
+++ Api/Admin/LogController.cs
@@ -0,0 +1,29 @@
+using Holism.Api;
+using Holism.Business;
+using Holism.Logs.Business;
+using Holism.Logs.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Holism.AdminApi.Api.Controllers
+{
+    public class LogController : Controller<LogView, Log>
+    {
+        public override ReadBusiness<LogView> ReadBusiness => new LogBusiness();
+        
+        public override Business<LogView, Log> Business => new LogBusiness();
+
+        [HttpPost]
+        public IActionResult Empty()
+        {
+            new LogBusiness().Empty();
+            return OkJson();
+        }
+
+        [HttpPost]
+        public IActionResult CreateTestLogs()
+        {
+            new LogBusiness().CreateTestLogs();
+            return OkJson();
+        }
+    }
+}
