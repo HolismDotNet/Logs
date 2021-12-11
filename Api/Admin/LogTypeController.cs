ea9b9e4887a85d110b95cfdef81f34c7f754088e
Saeed Nemati <sn.1361@gmail.com>
2021-12-11 21:38:47 +0330

    committed by script

--- Api/Admin/LogTypeController.cs
+++ Api/Admin/LogTypeController.cs
@@ -2,5 +2,5 @@ namespace Holism.Logs.AdminApi;
 
 public class LogTypeController : EnumController<Models.Type>
 {
-    public override EnumBusiness<Models.Type> EnumBusiness => new TypeBusiness();
+    public override EnumBusiness<Models.Type> EnumBusiness => new Logs.Business.TypeBusiness();
 }
